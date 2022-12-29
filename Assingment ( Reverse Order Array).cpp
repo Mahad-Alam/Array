0c6a9a3015570b8e7a219d1b730b443f9fb8d28a
Mahad-Alam <116580496+Mahad-Alam@users.noreply.github.com>
2022-12-30 00:05:54 +0500

    Add files via upload

---
+++ Assingment ( Reverse Order Array).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[6];
+    cout<<"Enter 6 Numbers :"<<endl;
+    for(int i=0;i<=5;i++)
+    cin>>arr[i];
+    cout<<"NON-REVERSED NUMBERS ARE:"<<endl;
+    for(int i=0;i<=5;i++)
+    cout<<arr[i]<<endl;
+    cout<<endl;
+    cout<<"REVERSED NUMBERS ARE:"<<endl;
+     for(int i=5;i>=0;i--)
+    cout<<arr[i]<<endl;
+  
+  return 0;
+
+}
